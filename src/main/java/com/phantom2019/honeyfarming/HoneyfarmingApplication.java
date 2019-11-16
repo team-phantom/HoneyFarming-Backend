260fd8d64ce2d828bac5e9298e4a3f9314dd4b6b
Jaehoon Kim <by09115@outlook.kr>
2019-11-16 23:38:06 +0900

    SET: project structure

---
+++ src/main/java/com/phantom2019/honeyfarming/HoneyfarmingApplication.java
@@ -0,0 +1,13 @@
+package com.phantom2019.honeyfarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneyfarmingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyfarmingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/phantom2019/honeyfarming/HoneyfarmingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phantom2019.honeyfarming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyfarmingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
